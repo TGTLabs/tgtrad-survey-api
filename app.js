23e561aa178f0c82191ab501d0fa79a79fb31ea9
Luke Gesior <luke.gesior@target.com>
2014-08-15 10:12:54 -0500

    app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var dotenv = require('dotenv');
+dotenv.load();
+
+var thisPackage = require('./package');
+var express = require("express");
+var serveStatic = require('serve-static');
+var morgan = require('morgan');
+var compress = require('compression');
+
+// create server
+var app = express();
+app.set('title', thisPackage.description);
+
+// configure logging
+var loggingFormat = 'remote=:remote-addr ":method :url HTTP/:http-version" status=:status length=:res[content-length] responseTime=:response-time request_id=:req[X-Request-ID] referrer=":referrer" userAgent=":user-agent"';
+app.use(morgan(loggingFormat));
+
+// enable gzip compression
+app.use(compress());
+
+// view engine setup
+app.set('views', (__dirname + '/views'));
+app.set('view engine', 'jade');
+
+// setup routes
+require('./routes/heatmap')(app);
+
+// static content
+app.use('/build', serveStatic(__dirname + '/build'));
+
+// start server
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+    console.log("%s, version %s. Listening on %s", app.get('title'), thisPackage.version, port);
+});
