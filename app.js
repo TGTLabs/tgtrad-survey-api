e6e3fdc21b5d5901f4c885a32171fb72b5f7b550
Luke Gesior <luke.gesior@target.com>
2014-09-08 16:51:49 -0500

    adding X-KEY header to all api requests to validate user

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ var dotenv = require('dotenv');
 var passport = require('passport');
 var User = require('./shared/models/User.js');
 var GoogleStrategy = require('passport-google').Strategy;
+var crypto = require('crypto');
 
 dotenv.load();
+
+
 
 var thisPackage = require('./package');
 var server = require('./lib/restify')(thisPackage.description);
@@ -20,7 +23,7 @@ require('./shared/models');
 var restifyMongoose = require('restify-mongoose');
 
 server.use(passport.initialize());
-server.use(passport.session());
+// server.use(passport.session());
 
 // passport.serializeUser(User.serializeUser);
 // passport.deserializeUser(User.deserializeUser);
@@ -37,18 +40,22 @@ passport.deserializeUser(function(obj, done) {
 
 passport.use(new GoogleStrategy({
     returnURL: 'http://localhost:5000/auth/google/return',
-    realm: 'http://localhost:5000/'
+    realm: 'http://localhost:5000/',
+    stateless: true,
+    pape: {
+      maxAuthAge: 600
+    }
   },
   function(identifier, profile, done) {
     // asynchronous verification, for effect...
-    process.nextTick(function () {
+    process.nextTick(function() {
 
       // To keep the example simple, the user's Google profile is returned to
       // represent the logged-in user.  In a typical application, you would want
       // to associate the Google account with a user record in your database,
       // and return that user instead.
       profile.identifier = identifier;
-      return done(null, profile);
+       return done(null, profile);
     });
   }
 ));
@@ -59,7 +66,6 @@ passport.use(new GoogleStrategy({
  *********************/
 require('./api').register(server);
 
-server.get("/eh", getVersion);
 
 // GET /auth/google
 //   Use passport.authenticate() as route middleware to authenticate the
@@ -67,10 +73,24 @@ server.get("/eh", getVersion);
 //   the user to google.com.  After authenticating, Google will redirect the
 //   user back to this application at /auth/google/return
 server.get('/auth/google',
-  passport.authenticate('google', { failureRedirect: '/login' }),
+  passport.authenticate('google', {
+    failureRedirect: '/login'
+  }),
   function(req, res) {
     res.redirect('/');
   });
+
+
+server.get("/test", passport.authenticate('google', {
+  session: false
+}), function(req, res, next) {
+  res.send("authenticated");
+});
+
+// server.get("/test",isAuthenticated, function(req,res,next){
+// console.log("got here yo");
+// res.send("authenticated");
+// });
 
 // GET /auth/google/return
 //   Use passport.authenticate() as route middleware to authenticate the
@@ -78,29 +98,45 @@ server.get('/auth/google',
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
 server.get('/auth/google/return',
-  passport.authenticate('google', { failureRedirect: '/login' }),
+  passport.authenticate('google', {
+    failureRedirect: '/login'
+  }),
   function(req, res) {
-    res.send('did something');
-    res.redirect('/');
-  });
-/*********************
- *  Resource functions
- *********************/
-function getVersion (req, res, next) {
-  res.cache('public', { maxAge: 300});
-  res.send({ name: "eh", version: "pkg.version"});
 
-  next();
-}
+    res.send(req.user.identifier);
+    //write email to user database
+    //search database for
+    //fulfill initial request if there was one ()
+    var cipher = crypto.createCipher('aes-256-cbc','FortunaMajor');
+    var xKey = {
+      identity: req.user.identifier,
+      retryAuth: "now"
+    };
+    var xKeyString = JSON.stringify(xKey);
+    var crypted = cipher.update(xKeyString,'utf8','hex');
+    crypted += cipher.final('hex');
+    res.send(crypted);
+    var decipher = crypto.createDecipher('aes-256-cbc','FortunaMajor');
+    var dec = decipher.update(crypted,'hex','utf8');
+    dec += decipher.final('utf8');
+    var parseText = JSON.parse(dec);
+
+    // console.log(dec);
+  });
+
 
 // START SERVER
 var port = process.env.PORT || 5000;
-server.listen(port, function () {
-  console.log("%s, version %s. Listening on %s", server.name, thisPackage.version, port);
+server.listen(port, function() {
+  console.log("%s, version %s. Listening on %s", server.name, thisPackage.version,
+    port);
 });
 
 
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login');
+function isAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    next(new Error(401));
+  }
 }
